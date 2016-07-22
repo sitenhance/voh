28c78af18357c5bd907e0ac0f0c670f139ad1d59
Halbert Rivera <Halbert Rivera>
2016-07-22 13:23:05 -0500

    Created stylists

--- js/app/stylists/stylist.controller.js
+++ js/app/stylists/stylist.controller.js
@@ -1,11 +1,13 @@
 (function(){
     vohApp.controller('stylistCtrl', ['$scope', function($scope) {
-        console.log(stylists);
 
         //Find Stylists That Are Featured
         function findFeaturedStylists() {
             var featuredStylists = _.filter(stylists, { 'featured': true });
+            return featuredStylists;
         }
+
+        $scope.stylists = findFeaturedStylists();
         
         //Find Stylists that do certain haircuts
         function findSpecialties(pickedStyle) {
@@ -24,6 +26,8 @@
 
                 })
                 .value();
+
+            return specialStylists;
         }
 
         findSpecialties('bobcuts');
@@ -42,27 +46,21 @@
                 }
             })
             .value();
-            console.log(stylistsInLocation);
+            return stylistsInLocation;
         }
         findLocation(locationArray[0], locationArray[1]);
 
         //Move featured stylists to the top
         function featuredFirst(pikachu) {
             var stylistsArray = [];
-            // stylistsArray = pikachu;
-            // console.log(stylistsArray);
             var formattedStylists = _.chain(pikachu).filter(function(item) {
-                console.log('Looping through stylists: ', item);
                 if(item.featured === true) {
                     return stylistsArray.push(item);
-                    //  return pikachu.unshift(item);
                 }
             }).value();
             pikachu.unshift(formattedStylists);
              return _.flattenDeep(pikachu);
         }
-
-        console.log(featuredFirst(stylists));
 
     }]);
 }());
