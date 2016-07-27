5022a0e327465cbc5bed74c8870b24dd83376a3a
Halbert Rivera <Halbert Rivera>
2016-07-27 11:10:02 -0500

    Added Routing to the application

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,29 @@
-var vohApp = angular.module('vohApp', []);
+var vohApp = angular.module('vohApp', ['ui.router']);
+
+vohApp.config(function($locationProvider) {
+    // $locationProvider.html5Mode(true);
+});
+
+//Application Routing
+vohApp.config(function($stateProvider, $urlRouterProvider) {
+
+    //Default to home page
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'js/app/home/home.html'
+        })
+        .state('find-stylists', {
+            url: '/find-stylists',
+            templateUrl: 'js/app/stylists/find-stylists.html',
+            controller: 'stylistCtrl'
+        })
+        .state('stylist', {
+            url: '/stylist/:id',
+            templateUrl: '../single-stylist.html'
+        });
+
+
+});
