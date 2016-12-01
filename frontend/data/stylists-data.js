83f76a28888f51f6a293eaa2efb75c222f4666e7
Halbert Rivera <hrivera@outlook.com>
2016-12-01 11:12:20 -0600

    Front End Mock Data

---
+++ frontend/data/stylists-data.js
@@ -0,0 +1,150 @@
+var stylistsList = [
+    {
+        'id': 1,
+        'name': 'Brenda Gee',
+        'city': 'Atlanta',
+        'state': 'GA',
+        'specialties': ['bobcuts', 'buns', 'braids'],
+        'bio': 'Creative and spirited are two words to describe hair stylist Brenda Gee. Well known for her innovation hair designs, and educational /motivational classes this New York native will never let you second guess her talents as a hair stylist or as a business woman. Excelling in the hair care industry for over 30 years, Brenda’s Talents are based on a solid foundation of education and product knowledge. Brenda Graduated from Robert Fiance 1979 she went on to style trends and Black Hair is in Brooklyn N.Y. and taught at Wilfred Academy. After moving to Miami Brenda became the Premiere stylist at Glemby Salon in 1985. Her entrerperunal spirit surfaced in 1989 when she opened Turning Heads in North Miami, In 1991 she began traveling the United States, Canada and the Caribbean as an educational Artist for Dudley Products, in 2001 she went on to work for Oster Products and is presently on Mazza Style Team. In 2000 Brenda opened Brenda Gee Salon in Hollywood Fl, and began styling the tresses of Mariah Careys Rainbow tour and Venus and Serena williams, she also work on music videos such as trick Daddy, Little Romeo, Jagged Edge/ Nelly and many more. in 2005 Brenda relocated to Atlanta Ga and Opened Brenda Gee Salon in Alpharetta. &quot; I love healthy hair that moves and feels like silk weather short or long, thin or course its all beautiful! My job is transforming a clients hair right before their eyes now thats gratification!&quot',
+        'images': [
+            {
+                'source': '/data/brenda_gee/brenda_1.jpg',
+                'caption': 'My caption one'
+            },
+            {
+                'source': '/data/brenda_gee/brenda_2.jpg',
+                'caption': 'My caption two'
+            },
+            {
+                'source': '/data/brenda_gee/brenda_3.jpg',
+                'caption': 'My caption three'
+            },
+            {
+                'source': '/data/brenda_gee/brenda_4.jpg',
+                'caption': 'My caption four'
+            },
+            {
+                'source': '/data/brenda_gee/brenda_5.jpg',
+                'caption': 'My caption five'
+            },
+            {
+                'source': '/data/brenda_gee/brenda_6.jpg',
+                'caption': 'My caption six'
+            }
+        ],
+        'featured': true
+    },
+    {
+        'id': 2,
+        'name': 'Tierra Holly Barnes',
+        'city': 'Atlanta',
+        'state': 'GA',
+        'specialties': ['buns', 'braids'],
+        'bio': 'I&apos;ve been interested in hair since my Barbie days, cutting and styling my dollies. I was the girl in school that used to play in everyone&apos;s hair. And as a teenager braided hair to earn some cash. When I was 16 my cousin got me a job as an assistant at her cousins salon. That was my job all through high school and college. I attended the university of Illinois at Chicago from 2005-2009, majoring in business administration. It was my senior year in college when Paul Mitchell the school Chicago was built directly across the street from my campus. I decided to take a tour and I fell in love with how professional the salon was. I had always wanted a career and until then I didn&apos;t see that being a cosmetologist could be a career. I was so excited about it I enrolled in Paul Mitchell while still finishing my degree at UIC.  After I graduated from UIC I decided to move to Atlanta where I transferred from Paul Mitchell to Roberts school of cosmetology. I graduated and got my license in 2011 and have been doing hair professionally since then. I have worked with, been mentored by, and taken classes from some of the best in the industry such as Anthony Cutz, Khimandi, Cynthia Lumzy and HairbyChantellen to name a few. I aspire and take pride in being a well versed stylist who can transition my clients from many different hair trends.',
+        'images': [
+            {
+                'source': '/data/tierra_holly_barnes/thb_1.png',
+                'caption': 'My caption one'
+            },
+            {
+                'source': '/data/tierra_holly_barnes/thb_2.png',
+                'caption': 'My caption two'
+            },
+            {
+                'source': '/data/tierra_holly_barnes/thb_3.png',
+                'caption': 'My caption three'
+            },
+            {
+                'source': '/data/tierra_holly_barnes/thb_4.png',
+                'caption': 'My caption four'
+            },
+            {
+                'source': '/data/tierra_holly_barnes/thb_5.png',
+                'caption': 'My caption five'
+            },
+            {
+                'source': '/data/tierra_holly_barnes/thb_6.png',
+                'caption': 'My caption six'
+            }
+        ],
+        'featured': true
+    },
+    {
+        'id': 3,
+        'name': 'Kenya Kirk',
+        'city': 'Austin',
+        'state': 'TX',
+        'specialties': ['buns', 'braids', 'undercuts'],
+        'bio': 'As a personal stylist, those fortunate enough to be on her book (which reads like a who&apos;s who of the fashion industry), she allows her talents to go further than just the style, instead she extends her knowledge to her clients, giving them essential "hot tips" to maintain and flaunt their custom styles. Living her passion and dream, Kenya wanted to share her joy even further, in the fall of 2013 she started Dreamz for Diamondz. A non-profit organization making wigs and partial wigs for young children undergoing chemotherapy.  With a vow that "no child should have to pay for being beautiful as they are", Kenya works tirelessly campaigning for donations for this cause with tremendous success. Kenya&apos;s strong desire, empowering attitude and innovative mindset, keeps her fresh and inspirational for everyone she comes in contact with.  If Kenya isn&apos;t at her salon making someone fabulous, you can find her in a classroom or at a seminar in any country at anytime, at a fashion show, photoshoot, or a charitable event. Giving back and staying active is important for Kenya and holds a safe place in her heart. Education is key in her success  and journey; learning how to be better, new techniques and perfecting her craft, so she can become better at what she does....',
+        'images': [
+            {
+                'source': '/data/kenya_kirk/kk_1.png',
+                'caption': 'My caption one'
+            },
+            {
+                'source': '/data/kenya_kirk/kk_2.png',
+                'caption': 'My caption two'
+            },
+            {
+                'source': '/data/kenya_kirk/kk_3.png',
+                'caption': 'My caption three'
+            },
+            {
+                'source': '/data/kenya_kirk/kk_4.png',
+                'caption': 'My caption four'
+            },
+            {
+                'source': '/data/kenya_kirk/kk_5.png',
+                'caption': 'My caption five'
+            },
+            {
+                'source': '/data/kenya_kirk/kk_6.png',
+                'caption': 'My caption six'
+            }
+        ],
+        'featured': true
+    },
+    {
+        'id': 4,
+        'name': 'Chaundra',
+        'city': 'Dallas',
+        'state': 'TX',
+        'specialties': ['buns', 'braids', 'undercuts'],
+        'bio': 'As a personal stylist, those fortunate enough to be on her book (which reads like a who&apos;s who of the fashion industry), she allows her talents to go further than just the style, instead she extends her knowledge to her clients, giving them essential "hot tips" to maintain and flaunt their custom styles. Living her passion and dream, Kenya wanted to share her joy even further, in the fall of 2013 she started Dreamz for Diamondz. A non-profit organization making wigs and partial wigs for young children undergoing chemotherapy.  With a vow that "no child should have to pay for being beautiful as they are", Kenya works tirelessly campaigning for donations for this cause with tremendous success. Kenya&apos;s strong desire, empowering attitude and innovative mindset, keeps her fresh and inspirational for everyone she comes in contact with.  If Kenya isn&apos;t at her salon making someone fabulous, you can find her in a classroom or at a seminar in any country at anytime, at a fashion show, photoshoot, or a charitable event. Giving back and staying active is important for Kenya and holds a safe place in her heart. Education is key in her success  and journey; learning how to be better, new techniques and perfecting her craft, so she can become better at what she does....',
+        'images': [
+            {
+                'source': '/data/chaundra/chaundra_1.jpg',
+                'caption': 'My caption one'
+            },
+            {
+                'source': '/data/chaundra/chaundra_2.jpg',
+                'caption': 'My caption two'
+            },
+            {
+                'source': '/data/chaundra/chaundra_3.jpg',
+                'caption': 'My caption three'
+            },
+            {
+                'source': '/data/chaundra/chaundra_4.jpg',
+                'caption': 'My caption four'
+            },
+            {
+                'source': '/data/chaundra/chaundra_5.jpg',
+                'caption': 'My caption five'
+            },
+            {
+                'source': '/data/chaundra/chaundra_6.jpg',
+                'caption': 'My caption six'
+            },
+            {
+                'source': '/data/chaundra/chaundra_7.jpg',
+                'caption': 'My caption seven'
+            },
+            {
+                'source': '/data/chaundra/chaundra_8.jpg',
+                'caption': 'My caption eight'
+            },
+        ],
+        'featured': true
+    }
+];
